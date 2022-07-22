449b6a7d806272d0a317df0bde940d2171b245df
Daniel Weiß <dweiss@exa-online.de>
2022-07-22 09:47:34 +0200

    adapt build script

--- build.sh
+++ build.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/local/bin/bash
 
 #operatingSystems=("aix" "android" "darwin" "dragonfly" "freebsd" "illumos" "ios" "js" "linux" "netbsd" "openbsd" "plan9" "solaris" "windows")
-operatingSystems=("android" "freebsd" "ios" "linux" "netbsd" "openbsd" "windows")
 #archs=("386" "amd64" "arm" "arm64" "mips" "mips64" "mips64le" "mipsle" "ppc64" "ppc64le" "riscv64" "s390x" "wasm")
-archs=("386" "amd64" "arm" "arm64")
 
-for i in "${!operatingSystems[@]}"
+declare -A arch_os_pairs=( ["android"]="arm64" ["windows"]="386;amd64;arm;arm64" ["darwin"]="amd64;arm64" ["dragonfly"]="amd64" ["freebsd"]="386;amd64;arm;arm64" ["illumos"]="amd64" ["ios"]="amd64" ["linux"]="386;amd64;arm;arm64;mips;mips64;mips64le;riscv64;s390x" ["netbsd"]="386;amd64;arm;arm64" ["openbsd"]="386;amd64;arm;arm64;mips64" ["solaris"]="amd64" )
+
+for os in "${!arch_os_pairs[@]}"
 do
-  for j in "${!archs[@]}"
+  IFS=";" read -r -a archs <<< "${arch_os_pairs[$os]}"
+  for arch in "${archs[@]}"
   do
-    env GOOS=${operatingSystems[i]} GOARCH=${archs[j]} go build -o "dist/lut_apply.${operatingSystems[i]}_${archs[j]}"
-    printf '%s_%s\n' "${operatingSystems[i]}" "${archs[j]}"
+    printf 'building %s %s -> ' "${os}" "${arch}"
+    if env GOOS=$os GOARCH=$arch go build -o "dist/lut_apply.${os}_${arch}" > /dev/null 2>&1 ; then
+      printf 'built\n' "$os" "$arch"
+    else
+      printf 'failed\n' "$os" "$arch"
+    fi
   done
 done
-# Properly formatting the total computed number of combinations that is:
-# number of elements in array1 times number of elements in array2
-printf 'Total number of combinations: %d\n' "$((${#operatingSystems[@]} * ${#archs[@]}))"
